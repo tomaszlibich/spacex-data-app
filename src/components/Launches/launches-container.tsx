b51f476dfaa3eff3a1cf60715a0bfe49818f4216
Tomasz Libich <tomaszlibich@ip-192-168-1-31.eu-west-1.compute.internal>
2021-09-02 12:48:28 +0100

    Remove console entry

--- src/components/Launches/launches-container.tsx
+++ src/components/Launches/launches-container.tsx
@@ -24,7 +24,7 @@ export const LaunchesContainer = () => {
           const fetchedLaunches = await getLaunches();
 
           fetchedLaunchesRef.current = fetchedLaunches;
-          console.warn("LAUNCHES", fetchedLaunches);
+
           setLaunches(fetchedLaunches);
         } catch (e) {
           console.error(e);
